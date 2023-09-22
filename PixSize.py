c77032ae3a2f534749c373430b0b3849e552098c
Andrew Fleck <144138727+afleck18@users.noreply.github.com>
2023-09-22 10:22:58 -0400

    Adjusted ESF to automatically get best fit for data.

--- PixSize.py
+++ PixSize.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import requests
+from io import BytesIO
+from PIL import Image
 
-bwPhoto = cv2.imread('Camera.png',cv2.IMREAD_GRAYSCALE)
+url = 'https://raw.githubusercontent.com/adtacharya/pix-modulation-functions/main/camera.png'
+response = requests.get(url)
+bwPhoto = Image.open(BytesIO(response.content)).convert('L')
+bwPhoto = np.array(bwPhoto) 
 bwPhoto = bwPhoto.astype(np.float64)
 
-blurPhoto = cv2.imread('CameraBlur.png',cv2.IMREAD_GRAYSCALE)
+url = 'https://raw.githubusercontent.com/adtacharya/pix-modulation-functions/main/CameraBlur.png'
+response = requests.get(url)
+blurPhoto = Image.open(BytesIO(response.content)).convert('L')
+blurPhoto = np.array(blurPhoto) 
 blurPhoto = blurPhoto.astype(np.float64)
 
 plt.figure()
@@ -26,15 +29,25 @@ selected_line = bwPhoto[140:215,210]
 selected_line1 = blurPhoto[143:218,210]
 pixel = np.arange(1, 76,1)
 
-a = 166.2
-b = 33.14
-c = 0.6217
-d = 16.17
+def ESF(pixel,a,b,c,d):
+    """
+    Compute the Edge Spread Function from given parameters
+    """
+    esf = a/(np.exp((pixel-b)/c)+1)+d
 
-a1 = 168.3
-b1 = 33.35
-c1 = 3.508
-d1 = 14.46
+    return esf
+
+parameters, covarience = curve_fit(ESF,pixel,selected_line)
+a = parameters[0]
+b = parameters[1]
+c = parameters[2]
+d = parameters[3]
+
+par, covar = curve_fit(ESF,pixel,selected_line1)
+a1 = par[0]
+b1 = par[1]
+c1 = par[2]
+d1 = par[3]
 
 ESF =  a/(np.exp((pixel-b)/c)+1)+d
 ESF1 =  a1/(np.exp((pixel-b1)/c1)+1)+d1
@@ -45,17 +58,17 @@ plt.title("Edge Spread Function (ESF)")
 plt.ylabel("Pixel Intensity")
 plt.xlabel("Pixel Number")
 
-ESF = a/(np.exp((pixel-b)/c)+1)+d;
-ESF1 = a1/(np.exp((pixel-b1)/c1)+1)+d1;
+ESF = a/(np.exp((pixel-b)/c)+1)+d
+ESF1 = a1/(np.exp((pixel-b1)/c1)+1)+d1
 
-LSF_nu = abs(np.diff(ESF));
-LSF_nu1 = abs(np.diff(ESF1));
+LSF_nu = abs(np.diff(ESF))
+LSF_nu1 = abs(np.diff(ESF1))
 
-LSF_an = abs(a*np.exp((pixel-b)/c)/(c*(np.exp((pixel-b)/c)+1)**2));
-LSF_an1 = abs(a1*np.exp((pixel-b1)/c1)/(c1*(np.exp((pixel-b1)/c1)+1)**2));
+LSF_an = abs(a*np.exp((pixel-b)/c)/(c*(np.exp((pixel-b)/c)+1)**2))
+LSF_an1 = abs(a1*np.exp((pixel-b1)/c1)/(c1*(np.exp((pixel-b1)/c1)+1)**2))
 
-LSF_ave = np.mean(np.vstack((np.append(LSF_nu,0), LSF_an)),0);
-LSF_ave1 = np.mean(np.vstack((np.append(LSF_nu1,0), LSF_an1)),0);
+LSF_ave = np.mean(np.vstack((np.append(LSF_nu,0), LSF_an)),0)
+LSF_ave1 = np.mean(np.vstack((np.append(LSF_nu1,0), LSF_an1)),0)
 
 halfval = abs(0.5 - LSF_ave/max(LSF_ave))
 halfval1 = abs(0.5 - LSF_ave1/max(LSF_ave1))
